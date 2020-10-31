92fc67233b614b1dcf5a871742a6ed74069a1cdd
JGil <calterat@gmail.com>
2020-10-31 00:36:46 -0500

    added loop to make task slots

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,39 @@
 // the current day is displayed at the top of the calendar
 
-let date = moment();
+let today = moment();
 
-$("#currentDay").text(date.format("dddd, MMMM Do"));
+let hourStart = moment().hour(9);
 
+$("#currentDay").text(today.format("dddd, MMMM Do"));
 
 // create time blocks for standard business hours
+
+const createHour = (hour) => {
+    let newRow = $("<div>")
+        .addClass("row");
+
+    let newHour = $("<div>")
+        .addClass("hour col-1 pt-3")
+        .text(hour);
+
+    let newTask = $("<textarea>")
+        .addClass("col-10");
+
+    let newSave = $("<button>")
+        .addClass("saveBtn col-1")
+
+    newRow.append(newHour).append(newTask).append(newSave);
+    
+    $(".container").append(newRow);
+}
+
+for (i=0; i<9; ++i) {
+    if (i===0) {
+        createHour(hourStart.format("hA"));
+    } else {
+        createHour(hourStart.add(1,'h').format("hA"));
+    }
+}
 
 // color-code blocks to indicate past, present, or future
 
