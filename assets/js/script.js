855a7ccdb8e10ba0c7859fc1f2a86ee656d79704
JGil <calterat@gmail.com>
2020-10-31 02:34:05 -0500

    got to pulling values from textarea

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 // the current day is displayed at the top of the calendar
 
 let today = moment();
-
 let hourStart = moment().hour(9);
+let tasks = [];
 
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
@@ -26,17 +26,21 @@ const createHour = (hour) => {
     let newRow = $("<div>")
         .addClass("row");
 
+    // creates hour column
     let newHour = $("<div>")
         .addClass("hour col-1 pt-3")
         .text(hour);
 
+    // click into timeblock to enter event or edit it
     let newTask = $("<textarea>")
         .addClass("col-10")
         .attr("data-id", i);
 
+    // creates save button with icon from fontawesome
     let newSave = $("<button>")
         .addClass("saveBtn col-1")
-        .attr("data-id", i);
+        .attr("data-id", i)
+        .html("<i class='fas fa-save'></i>");
 
     auditTask(newTask);
 
@@ -53,8 +57,21 @@ for (i=0; i<9; ++i) {
     }
 }
 
-// click into timeblock to enter event or edit it
-
 // click save button to store in localStorage
+
+const saveTask = (event) => {
+
+    let dataId = event.target.getAttribute("data-id");
+    let taskInfo = $(`textarea[data-id='${dataId}']`).val();
+
+    let tempTask = {
+        id: dataId,
+        info: taskInfo
+    };
+
+    tasks.push(tempTask);
+};
+
+$(".container").on("click", "button", saveTask);
 
 // refresh shows persistent data
