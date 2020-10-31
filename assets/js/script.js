258a5175302f392abfd78630d99789359e015cc0
Calterat <Calterat@gmail.com>
2020-10-30 20:08:48 -0500

    assessed existing code

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// the current day is displayed at the top of the calendar
+
+// create time blocks for standard business hours
+
+// color-code blocks to indicate past, present, or future
+
+// click into timeblock to enter event or edit it
+
+// click save button to store in localStorage
+
+// refresh shows persistent data
